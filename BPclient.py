5e6f90b6eafacafc96548a6c709ea7899c08bd69
juesexiangyang <1796539204@qq.com>
2016-06-01 22:28:36 +0900

    添加了try,except退出判断

--- BPclient.py
+++ BPclient.py
@@ -3,14 +3,20 @@ from socket import *
 client = socket(AF_INET,SOCK_DGRAM)
 
 addr = ('10.19.22.48',50000)
+try:
+    while True:
+        sendmsg = raw_input('me:')
+        if not sendmsg:
+            pass
+        client.sendto(sendmsg,addr)
 
-while True:
-    sendmsg = raw_input('me:')
-    if not sendmsg:
-        pass
-    client.sendto(sendmsg,addr)
-
-    recvmsg,addr = client.recvfrom(1024)
-    if not recvmsg:
-        pass
-    print 'others:',recvmsg
+        recvmsg,addr = client.recvfrom(1024)
+        if not recvmsg:
+            pass
+        print 'others:',recvmsg
+except EOFError:
+    print 'catched EOFError'
+    client.close()
+except KeyboardInterrupt:
+    print 'catched KeyboardInterrupt'
+    client.close()
--- BPserver.py
+++ BPserver.py
@@ -3,21 +3,26 @@ from socket import *
 server = socket(AF_INET,SOCK_DGRAM)
 
 server.bind(('0.0.0.0',50000))
-
-while True:
-    #print 'waiting for message...'
+try:
+    while True:
+        #print 'waiting for message...'
     
-    recvmsg,addr = server.recvfrom(1024)
-    #print '...connected from :',addr
+        recvmsg,addr = server.recvfrom(1024)
+        #print '...connected from :',addr
 
-    if recvmsg:
-        print 'others:',recvmsg    
+        if recvmsg:
+            print 'others:',recvmsg    
 
-    sendmsg = raw_input('me:')
-    if not sendmsg:
-        pass
-    server.sendto(sendmsg,addr)
-    
+        sendmsg = raw_input('me:')
+        if not sendmsg:
+            pass
+        server.sendto(sendmsg,addr)
+except EOFError:
+    print 'catched EOFError'
+    server.close()
+except KeyboardInterrupt:
+    print 'catched KeyboardInterrupt'
+    server.close()
 
 
 
